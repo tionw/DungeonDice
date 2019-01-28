82f2c95c97a9082d9642d6495d20ee825050ba16
user150978 <user150978@ny030.local>
2019-01-28 17:48:50 -0500

    Initial Commit

---
+++ DungeonDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DungeonDice
+//
+//  Created by user150978 on 1/28/19.
+//  Copyright © 2019 Tion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
